50f3bd5207b3d951c90aae96b68cf77b43e4f9e8
Gemma Black <gemma@rentr.com>
2018-12-25 02:11:03 +0000

    Listings vs article modifications

--- pages/article.vue
+++ pages/article.vue
@@ -1,96 +1,49 @@
 <template>
-  <div id="page" class="relative hidden page pa3">
+  <div id="page" class="relative hidden page page--small pa3">
+    <span class="dt w-100 h6-m"></span>
     <span class="tu grey fs1 ls2 mb2 dib">Latest</span>
     <h1
-      class="fs5 fs7-ns fs9-m lh5 lh7-ns lh10-m fwl grey-dark mb2 maxw-90-m maxw-75-xl"
+      class="fs5 fs7-ns fs9-m lh5 lh7-ns lh10-m fwl grey-dark mb2 maxw-90-m"
     >Because great fashion do never follow trends</h1>
     <div class="grey fs1 oblique mb3">December 25, 2018</div>
     <span class="w6 h-thin bg-grey dt mt3 mb3"></span>
     <h2
-      class="grey lh3 mb3 maxw-80-ns maxw-60-l"
+      class="grey-dark o-80 lh3 mb3 maxw-80-ns maxw-60-l"
     >Find out the latest and freshest trends that came out in Paris Week</h2>
-    <span
-      class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2"
-    >read</span>
-
-    <ul class="df-ns flex-wrap-ns two-col-ns three-col-xl">
-      <li>
-        <span class="dt h-thin bg-blue w-100 o-60 mt6 mb6"></span>
-        <h3
-          class="fs5 lh5 fwl grey-dark mb2"
-        >Singers are singers because they enjoy excercising their human instrument</h3>
-        <div class="grey fs1 oblique mb3">December 25, 2018</div>
-        <span class="w6 h-thin bg-grey dt mt3 mb3"></span>
-        <p class="grey lh3 mb3">Find out the latest and freshest trends that came out in Paris Week</p>
-        <span
-          class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2 mb3"
-        >read</span>
-      </li>
-
-      <li>
-        <span class="dt h-thin bg-blue w-100 o-60 mt6 mb6"></span>
-        <h3
-          class="fs5 lh5 fwl grey-dark mb2"
-        >Singers are singers because they enjoy excercising their human instrument</h3>
-        <div class="grey fs1 oblique mb3">December 25, 2018</div>
-        <span class="w6 h-thin bg-grey dt mt3 mb3"></span>
-        <p class="grey lh3 mb3">Find out the latest and freshest trends that came out in Paris Week</p>
-        <span
-          class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2 mb3"
-        >read</span>
-      </li>
-
-      <li>
-        <span class="dt h-thin bg-blue w-100 o-60 mt6 mb6"></span>
-        <h3
-          class="fs5 lh5 fwl grey-dark mb2"
-        >Singers are singers because they enjoy excercising their human instrument</h3>
-        <div class="grey fs1 oblique mb3">December 25, 2018</div>
-        <span class="w6 h-thin bg-grey dt mt3 mb3"></span>
-        <p class="grey lh3 mb3">Find out the latest and freshest trends that came out in Paris Week</p>
-        <span
-          class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2 mb3"
-        >read</span>
-      </li>
-      <li>
-        <span class="dt h-thin bg-blue w-100 o-60 mt6 mb6"></span>
-        <h3
-          class="fs5 lh5 fwl grey-dark mb2"
-        >Singers are singers because they enjoy excercising their human instrument</h3>
-        <div class="grey fs1 oblique mb3">December 25, 2018</div>
-        <span class="w6 h-thin bg-grey dt mt3 mb3"></span>
-        <p class="grey lh3 mb3">Find out the latest and freshest trends that came out in Paris Week</p>
-        <span
-          class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2 mb3"
-        >read</span>
-      </li>
-
-      <li>
-        <span class="dt h-thin bg-blue w-100 o-60 mt6 mb6"></span>
-        <h3
-          class="fs5 lh5 fwl grey-dark mb2"
-        >Singers are singers because they enjoy excercising their human instrument</h3>
-        <div class="grey fs1 oblique mb3">December 25, 2018</div>
-        <span class="w6 h-thin bg-grey dt mt3 mb3"></span>
-        <p class="grey lh3 mb3">Find out the latest and freshest trends that came out in Paris Week</p>
-        <span
-          class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2 mb3"
-        >read</span>
-      </li>
-
-      <li>
-        <span class="dt h-thin bg-blue w-100 o-60 mt6 mb6"></span>
-        <h3
-          class="fs5 lh5 fwl grey-dark mb2"
-        >Singers are singers because they enjoy excercising their human instrument</h3>
-        <div class="grey fs1 oblique mb3">December 25, 2018</div>
-        <span class="w6 h-thin bg-grey dt mt3 mb3"></span>
-        <p class="grey lh3 mb3">Find out the latest and freshest trends that came out in Paris Week</p>
-        <span
-          class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2 mb3"
-        >read</span>
-      </li>
-    </ul>
+    <div class="df items-center">
+      <div class="maxw-big w-100">
+        <picture class="db u-aspect-ratio u-aspect-ratio--square bdr-50 hidden">
+          <img
+            class="u-aspect-ratio__inner"
+            style="object-fit: cover;"
+            src="https://images.unsplash.com/photo-1503593245033-a040be3f3c82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+          >
+        </picture>
+      </div>
+      <div class="grey-dark o-80 fs2 oblique tu ml3">Gemma</div>
+    </div>
+    <span class="dt h-thin bg-grey-dark w-100 w6-ns o-60 mt3 mb3"></span>
+    <div class="fs2 lh3 grey-dark o-80">
+      <p class="mb3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+        in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p class="mb3">
+        Duis aute irure dolor
+        in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <img
+        src="https://images.unsplash.com/photo-1545579534-b4634a69141a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2070&q=80"
+        class="w-100 h-au mb3"
+      >
+    </div>
   </div>
 </template>
 
--- pages/list.vue
+++ pages/list.vue
@@ -9,6 +9,88 @@
     <h2
       class="grey lh3 mb3 maxw-80-ns maxw-60-l"
     >Find out the latest and freshest trends that came out in Paris Week</h2>
+    <span
+      class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2"
+    >read</span>
+
+    <ul class="df-ns flex-wrap-ns two-col-ns three-col-xl">
+      <li>
+        <span class="dt h-thin bg-blue w-100 o-60 mt6 mb6"></span>
+        <h3
+          class="fs5 lh5 fwl grey-dark mb2"
+        >Singers are singers because they enjoy excercising their human instrument</h3>
+        <div class="grey fs1 oblique mb3">December 25, 2018</div>
+        <span class="w6 h-thin bg-grey dt mt3 mb3"></span>
+        <p class="grey lh3 mb3">Find out the latest and freshest trends that came out in Paris Week</p>
+        <span
+          class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2 mb3"
+        >read</span>
+      </li>
+
+      <li>
+        <span class="dt h-thin bg-blue w-100 o-60 mt6 mb6"></span>
+        <h3
+          class="fs5 lh5 fwl grey-dark mb2"
+        >Singers are singers because they enjoy excercising their human instrument</h3>
+        <div class="grey fs1 oblique mb3">December 25, 2018</div>
+        <span class="w6 h-thin bg-grey dt mt3 mb3"></span>
+        <p class="grey lh3 mb3">Find out the latest and freshest trends that came out in Paris Week</p>
+        <span
+          class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2 mb3"
+        >read</span>
+      </li>
+
+      <li>
+        <span class="dt h-thin bg-blue w-100 o-60 mt6 mb6"></span>
+        <h3
+          class="fs5 lh5 fwl grey-dark mb2"
+        >Singers are singers because they enjoy excercising their human instrument</h3>
+        <div class="grey fs1 oblique mb3">December 25, 2018</div>
+        <span class="w6 h-thin bg-grey dt mt3 mb3"></span>
+        <p class="grey lh3 mb3">Find out the latest and freshest trends that came out in Paris Week</p>
+        <span
+          class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2 mb3"
+        >read</span>
+      </li>
+      <li>
+        <span class="dt h-thin bg-blue w-100 o-60 mt6 mb6"></span>
+        <h3
+          class="fs5 lh5 fwl grey-dark mb2"
+        >Singers are singers because they enjoy excercising their human instrument</h3>
+        <div class="grey fs1 oblique mb3">December 25, 2018</div>
+        <span class="w6 h-thin bg-grey dt mt3 mb3"></span>
+        <p class="grey lh3 mb3">Find out the latest and freshest trends that came out in Paris Week</p>
+        <span
+          class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2 mb3"
+        >read</span>
+      </li>
+
+      <li>
+        <span class="dt h-thin bg-blue w-100 o-60 mt6 mb6"></span>
+        <h3
+          class="fs5 lh5 fwl grey-dark mb2"
+        >Singers are singers because they enjoy excercising their human instrument</h3>
+        <div class="grey fs1 oblique mb3">December 25, 2018</div>
+        <span class="w6 h-thin bg-grey dt mt3 mb3"></span>
+        <p class="grey lh3 mb3">Find out the latest and freshest trends that came out in Paris Week</p>
+        <span
+          class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2 mb3"
+        >read</span>
+      </li>
+
+      <li>
+        <span class="dt h-thin bg-blue w-100 o-60 mt6 mb6"></span>
+        <h3
+          class="fs5 lh5 fwl grey-dark mb2"
+        >Singers are singers because they enjoy excercising their human instrument</h3>
+        <div class="grey fs1 oblique mb3">December 25, 2018</div>
+        <span class="w6 h-thin bg-grey dt mt3 mb3"></span>
+        <p class="grey lh3 mb3">Find out the latest and freshest trends that came out in Paris Week</p>
+        <span
+          class="pointer fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 lh2 bc-transparent bg-blue tl ice ls2 mb3"
+        >read</span>
+      </li>
+    </ul>
   </div>
 </template>
 
