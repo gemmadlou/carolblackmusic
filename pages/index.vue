c04c5643a30057fe9be1b45885e98a8895d6465d
Gemma Black <gblackuk@googlemail.com>
2018-12-31 14:27:56 +0000

    Homepage copy updated and mobile nav implemented

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,17 @@
   <div id="page" class="relative hidden">
     <!-- page -->
     <section class="page pa3 pb6">
-      <h1 class="fs3 fwb white dib ls2">carolblackmusic</h1>
-
-      <span class="dt h10 w-100"></span>
-      <span class="dn dt-ns h10 w-100"></span>
-
       <div class="maxw-super">
-        <p class="tl fs2 ls2 mb1 white o-60">Website relaunch</p>
-        <h3 class="fs2 lh3 white mb2 ls1 o-90">
-          Coaching and practical advice for the
-          professional singer and entertainer.
-        </h3>
+        <p class="tl fs2 ls2 mb1 white o-60 lh3">30 years in music</p>
+        <h3
+          class="fs2 lh3 white mb2 ls1 o-90"
+        >Professional singer, songwriter, performer and vocal coach</h3>
         <span
           v-on:click="$store.commit('showBanner')"
-          class="pointer fwl fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 bc-white tl white ls2"
-        >get updates</span>
+          class="pointer fwl fs2 pa1 pl3 pr3 dib align-center bdr6 bs-solid bwa1 bc-white white ls2"
+        >Get updates</span>
+        
+        <span class="dt h10-m w-100"></span>
       </div>
     </section>
   </div>
